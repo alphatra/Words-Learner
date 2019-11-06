d6d650539893c51be57ba0b53035b9044c4a4388
alphatra <alpphatra@gmail.com>
2019-11-06 16:09:50 +0100

    zmiany z 06.11.2019(na laptop)

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:keyboard_avoider/keyboard_avoider.dart';
 class MyHomePage extends StatefulWidget {
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -9,10 +11,14 @@ class _MyHomePageState extends State<MyHomePage> {
   static String assetName1 = 'assets/svg/symbol.svg';
   static String assetName2 = 'assets/svg/symbol2.svg';
   bool _visible1 = true;
-  bool _visible2 = true;
+  bool _visible2 = false;
+  List<String> litems = [];
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+  final TextEditingController eCtrl = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
+    return Scaffold(backgroundColor: Colors.white,resizeToAvoidBottomPadding: true,
       body:  Stack(
         children: <Widget>[
           Positioned(
@@ -83,25 +89,30 @@ class _MyHomePageState extends State<MyHomePage> {
               alignment: Alignment.topCenter,
               child: FractionallySizedBox(
                 widthFactor: 0.7,
-                heightFactor: 0.4,
+                heightFactor: 0.45,
                 child: Container(
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  child: Scaffold(
+                    body: new Column(
                       children: <Widget>[
-                        GestureDetector(onTap: (){
-                          setState(() {
-                            _visible1 = !_visible1;
-                          });
-                        },child: AnimatedDefaultTextStyle(child: Text("Mode",textAlign: TextAlign.center), style: TextStyle(fontFamily: "NotoSans", fontSize: 16, color: _visible1 ? Colors.black26 : Colors.black), duration: Duration(milliseconds: 300))),
-                        Padding(padding: EdgeInsets.symmetric(horizontal: 16)),
-                        GestureDetector(onTap: (){
-                          setState(() {
-                            _visible2 = !_visible2;
-                          });
-                        },child: AnimatedDefaultTextStyle(child: Text("Worldlist",textAlign: TextAlign.center), style: TextStyle(fontFamily: "NotoSans", fontSize: 16, color: _visible2 ? Colors.black26 : Colors.black), duration: Duration(milliseconds: 300)))
+                        new TextField(
+                          controller: eCtrl,
+                          onSubmitted: (text) {
+                            litems.add(text);
+                            eCtrl.clear();
+                            setState(() {});
+                          },
+                        ),
+                        new Expanded(
+                            child: new ListView.builder
+                              (
+                                itemCount: litems.length,
+                                itemBuilder: (BuildContext ctxt, int Index) {
+                                  return new Text(litems[Index]);
+                                }
+                            )
+                        )
                       ],
-                    ),
+                    )
                   ),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(12)),
@@ -120,15 +131,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               )),),
-          Padding(padding: EdgeInsets.only(top: 430),child: Align(
-              alignment: Alignment.topCenter,
+          KeyboardAvoider(child: Padding(padding: EdgeInsets.only(bottom: 30),child: Align(
+              alignment: Alignment.bottomCenter,
               child: FractionallySizedBox(
-                widthFactor: 0.9,
-                heightFactor: 0.9,
+                widthFactor:  0.9,
+                heightFactor: 0.35,
                 child: Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.only(bottomRight: Radius.circular(30),bottomLeft: Radius.circular(30),topLeft: Radius.circular(15),topRight: Radius.circular(15)),
-                    color: Colors.blueGrey,
+                    color: Colors.white,
                     boxShadow: [
                       BoxShadow(
                         color: Colors.black.withOpacity(0.20),
@@ -142,7 +153,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                 ),
-              )),),
+              )),),),
+
 
         ],
       )
